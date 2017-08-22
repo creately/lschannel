ae7e2507bb61640b983c26b83d94e6838f603577
Muhammed Thanish <mnmtanish@gmail.com>
2017-08-22 15:29:59 +0530

    Implement send and recv methods

--- src/__tests__/index.spec.ts
+++ src/__tests__/index.spec.ts
@@ -1,13 +1,90 @@
-import { add } from '../';
+import { Channel } from '../';
+import { Observable } from 'rxjs';
 
-describe('add', () => {
-  it('should add 1 and 2', () => {
-    expect(add(1, 2)).toBe(1 + 2);
+describe('Channel', () => {
+  function prepare() {
+    const key = `test-key-${Math.random()}`;
+    const ch = new Channel<number>(key);
+    return { key, ch };
+  }
+
+  describe('send', () => {
+    it('should return undefined', () => {
+      const { ch } = prepare();
+      expect(ch.send(1)).toBe(undefined);
+    });
+
+    it('should modify the value for given key on local storage', () => {
+      const { key, ch } = prepare();
+      localStorage.setItem(key, 'previous-val');
+      expect(localStorage.getItem(key)).toBe('previous-val');
+      ch.send(1);
+      expect(localStorage.getItem(key)).not.toBe('previous-val');
+    });
   });
-  it('should add 1 and 3', () => {
-    expect(add(1, 3)).toBe(1 + 3);
-  });
-  it('should add 1 and 4', () => {
-    expect(add(1, 4)).toBe(1 + 4);
+
+  describe('recv', () => {
+    it('should return an observable', () => {
+      const { ch } = prepare();
+      expect(ch.recv()).toEqual(jasmine.any(Observable));
+    });
+
+    describe('return value', () => {
+      it('should emit when window emits a "storage" event with watched key', async () => {
+        const { key, ch } = prepare();
+        const promise = ch.recv().take(2).toArray().toPromise();
+        window.dispatchEvent(
+          new StorageEvent('storage', { key, newValue: JSON.stringify({ id: 'e1', data: 1 }) } as any)
+        );
+        window.dispatchEvent(
+          new StorageEvent('storage', { key, newValue: JSON.stringify({ id: 'e2', data: 2 }) } as any)
+        );
+        const result = await promise;
+        expect(result).toEqual([1, 2]);
+      });
+
+      it('should emit when the send method is called on the same instance', async () => {
+        const { ch } = prepare();
+        const promise = ch.recv().take(2).toArray().toPromise();
+        ch.send(1);
+        ch.send(2);
+        const result = await promise;
+        expect(result).toEqual([1, 2]);
+      });
+
+      it('should emit when the send method is called on the an instance with same key', async () => {
+        const { key, ch } = prepare();
+        const promise = ch.recv().take(2).toArray().toPromise();
+        const ch2 = new Channel(key);
+        ch2.send(1);
+        ch2.send(2);
+        const result = await promise;
+        expect(result).toEqual([1, 2]);
+      });
+
+      it('should not emit when window emits a "storage" event with different key', async () => {
+        const { key, ch } = prepare();
+        const promise = ch.recv().take(1).toArray().toPromise();
+        window.dispatchEvent(
+          new StorageEvent('storage', { key: 'not-key', newValue: JSON.stringify({ id: 'e1', data: 1 }) } as any)
+        );
+        window.dispatchEvent(
+          new StorageEvent('storage', { key, newValue: JSON.stringify({ id: 'e2', data: 2 }) } as any)
+        );
+        const result = await promise;
+        expect(result).toEqual([2]);
+      });
+
+      it('should not emit when localStorage is changed in current browser window', async () => {
+        const { key, ch } = prepare();
+        const promise = ch.recv().take(1).toArray().toPromise();
+        localStorage.setItem(key, JSON.stringify({ id: 'e1', data: 1 }));
+        window.dispatchEvent(
+          new StorageEvent('storage', { key, newValue: JSON.stringify({ id: 'e2', data: 2 }) } as any)
+        );
+        const result = await promise;
+        expect(result).toEqual([2]);
+      });
+    });
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,53 @@
-export function add(x: number, y: number = 0) {
-  if (x === 2) {
-    return y + x;
+import { Observable } from 'rxjs';
+
+export class Channel<T> {
+  // local
+  // local is an observable of lschannel events
+  public events: Observable<T>;
+
+  // constructor
+  // constructor creates a new LsChannel instance. The key parameter
+  // is mandatory and will be used as the localStorage key.
+  constructor(public key: string) {
+    this.events = Observable.fromEvent<any>(window, 'storage')
+      .filter(e => e.key === this.key)
+      .map(e => JSON.parse(e.newValue).data);
   }
-  return x + y;
+
+  // send
+  // send sets the value on localStorage for given key which
+  // will trigger a 'storage' event on other listening windows.
+  public send(data: T): void {
+    const event = { id: this.randomId(), data };
+    const value = JSON.stringify(event);
+    localStorage.setItem(this.key, value);
+    this.dispatch(value);
+  }
+
+  // recv
+  // recv returns an observable which will emit lschannel events
+  // triggered by other windows. Events created by this window
+  // will not trigger a "storage" event so it will not emit.
+  public recv(): Observable<T> {
+    return this.events;
+  }
+
+  // dispatch
+  // dispatch emits a "storage" event on window object. It will use
+  // the lschannel key as the key and given value as newValue.
+  private dispatch(value: string) {
+    const storageEvent = new StorageEvent('storage', { key: this.key, newValue: value, url: location.href });
+    window.dispatchEvent(storageEvent);
+  }
+
+  // randomId
+  // randomId creates a random string which will be used as event ids.
+  private randomId(): string {
+    let id = '';
+    while (id.length < 16) {
+      const r = Math.random().toString(36).slice(2);
+      id += r.slice(0, 16 - id.length);
+    }
+    return id;
+  }
 }
